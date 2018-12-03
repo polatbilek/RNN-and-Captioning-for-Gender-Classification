1196b3ad92a432bea7f5e2380ecba54ee8882909
ErhanSezerer <erhansezerer@gmail.com>
2018-12-03 16:36:31 +0300

    changes made to work with saved vectors

--- model.py
+++ model.py
@@ -9,40 +9,21 @@ class network(object):
 	############################################################################################################################
 	def __init__(self, embeddings):
 		with tf.device('/device:GPU:0'):
-			self.prediction = []
 
-			# create word embeddings
-			self.tf_embeddings = tf.Variable(tf.constant(0.0, shape=[embeddings.shape[0], embeddings.shape[1]]), trainable=False, name="tf_embeddings")
-			self.embedding_placeholder = tf.placeholder(tf.float32, [embeddings.shape[0], embeddings.shape[1]])
-			self.embedding_init = self.tf_embeddings.assign(self.embedding_placeholder)  # initialize this once  with sess.run when the session begins
-
-			# create GRU cells
-			with tf.variable_scope("tweet"):
-				self.cell_fw = tf.nn.rnn_cell.GRUCell(num_units=FLAGS.rnn_cell_size, activation=tf.sigmoid)
-				self.cell_bw = tf.nn.rnn_cell.GRUCell(num_units=FLAGS.rnn_cell_size, activation=tf.sigmoid)
-
-			# RNN placeholders
-			self.X = tf.placeholder(tf.int32, [FLAGS.batch_size*FLAGS.tweet_per_user, None])
+			# placeholders
+			self.textrnn_input = tf.placeholder(tf.float64, [FLAGS.batch_size, FLAGS.textrnn_size])
+			self.textcnn_input = tf.placeholder(tf.float64, [FLAGS.batch_size, FLAGS.textcnn_size])
+			self.imagernn_input = tf.placeholder(tf.float64, [FLAGS.batch_size, FLAGS.imagernn_size])
 			self.Y = tf.placeholder(tf.float64, [FLAGS.batch_size, FLAGS.num_classes])
-			self.sequence_length = tf.placeholder(tf.int32, [FLAGS.batch_size*FLAGS.tweet_per_user])
 			self.reg_param = tf.placeholder(tf.float32, shape=[])
 
 			# weigths
-			self.weights = {'fc1': tf.Variable(tf.random_normal([2 * FLAGS.rnn_cell_size, FLAGS.num_classes]), name="fc1-weights"),
-							'att1-w': tf.Variable(tf.random_normal([2 * FLAGS.rnn_cell_size, 2 * FLAGS.rnn_cell_size]), name="att1-weights"),
-							'att1-v': tf.Variable(tf.random_normal([2 * FLAGS.rnn_cell_size]), name="att1-vector"),
-							'att2-w': tf.Variable(tf.random_normal([2 * FLAGS.rnn_cell_size, 2 * FLAGS.rnn_cell_size]), name="att2-weights"),
-							'att2-v': tf.Variable(tf.random_normal([2 * FLAGS.rnn_cell_size]), name="att2-vector")}
+			self.weights = {'fc1': tf.Variable(tf.random_normal([FLAGS.textrnn_size + FLAGS.textcnn_size + FLAGS.imagernn_size, FLAGS.num_classes]), name="fc1-weights")}
 			# biases
-			self.bias = {'fc1': tf.Variable(tf.random_normal([FLAGS.num_classes]), name="fc1-bias-noreg"),
-						 'att1-w': tf.Variable(tf.random_normal([2 * FLAGS.rnn_cell_size]), name="att1-bias-noreg"),
-						 'att2-w': tf.Variable(tf.random_normal([2 * FLAGS.rnn_cell_size]), name="att2-bias-noreg")}
+			self.bias = {'fc1': tf.Variable(tf.random_normal([FLAGS.num_classes]), name="fc1-bias-noreg")}
 
 
 			# initialize the computation graph for the neural network
-			# self.rnn()
-			# self.rnn_with_attention()
-			self.captioning()
 			self.architecture()
 			self.backward_pass()
 
@@ -56,13 +37,12 @@ class network(object):
 	def architecture(self):
 
 		with tf.device('/device:GPU:0'):
-			#user level attention
-			self.att_context_vector_word = tf.tanh(tf.tensordot(self.attention_output, self.weights["att2-w"], axes=1) + self.bias["att2-w"])
-			self.attentions_word = tf.nn.softmax(tf.tensordot(self.att_context_vector_word, self.weights["att2-v"], axes=1))
-			self.attention_output_word = tf.reduce_sum(self.attention_output * tf.expand_dims(self.attentions_word, -1), 1)
+
+			#concatenation of features
+			self.concatenated_input = tf.concat([self.textrnn_input, self.textcnn_input, self.imagernn_input], 1)
 
 			# FC layer for reducing the dimension to 2(# of classes)
-			self.logits = tf.tensordot(self.attention_output_word, self.weights["fc1"], axes=1) + self.bias["fc1"]
+			self.logits = tf.matmul(self.concatenated_input, self.weights["fc1"]) + self.bias["fc1"]
 
 			# predictions
 			self.prediction = tf.nn.softmax(self.logits)
@@ -107,52 +87,6 @@ class network(object):
 
 
 
-	############################################################################################################################
-	def rnn(self):
-
-		with tf.device('/device:GPU:0'):	
-			# embedding layer
-			self.rnn_input = tf.nn.embedding_lookup(self.tf_embeddings, self.X)
-
-			# rnn layer
-			(self.outputs, self.output_states) = tf.nn.bidirectional_dynamic_rnn(self.cell_fw, self.cell_bw, self.rnn_input, self.sequence_length, dtype=tf.float32,scope="tweet")
-
-			# concatenate the backward and forward cells
-			self.rnn_output_raw = tf.concat([self.output_states[0], self.output_states[1]], 1)
-		
-			#reshape the output for the next layers
-			self.rnn_output = tf.reshape(self.rnn_output_raw, [FLAGS.batch_size, FLAGS.tweet_per_user, 2*FLAGS.rnn_cell_size])
-
-			return self.rnn_output
-
-
-
-
-
-
-
-    ############################################################################################################################
-	def rnn_with_attention(self):
-
-		with tf.device('/device:GPU:0'):
-			# embedding layer
-			self.rnn_input = tf.nn.embedding_lookup(self.tf_embeddings, self.X)
-
-			# rnn layer
-			(self.outputs, self.output_states) = tf.nn.bidirectional_dynamic_rnn(self.cell_fw, self.cell_bw, self.rnn_input, self.sequence_length, dtype=tf.float32,scope="tweet")
-
-			# concatenate the backward and forward cells
-			self.concat_outputs = tf.concat(self.outputs, 2)
-
-			# attention layer
-			self.att_context_vector = tf.tanh(tf.tensordot(self.concat_outputs, self.weights["att1-w"], axes=1) + self.bias["att1-w"])
-			self.attentions = tf.nn.softmax(tf.tensordot(self.att_context_vector, self.weights["att1-v"], axes=1))
-			self.attention_output_raw = tf.reduce_sum(self.concat_outputs * tf.expand_dims(self.attentions, -1), 1)
-
-			#reshape the output for the next layers
-			self.attention_output = tf.reshape(self.attention_output_raw, [FLAGS.batch_size, FLAGS.tweet_per_user, 2*FLAGS.rnn_cell_size])
-
-			return self.attention_output
 
 
 
@@ -162,28 +96,6 @@ class network(object):
 
 
 
-	############################################################################################################################
-	def captioning(self):
-		
-		with tf.device('/device:GPU:0'):
-			# embedding layer
-			self.rnn_input = tf.nn.embedding_lookup(self.tf_embeddings, self.X)
-
-			# rnn layer
-			(self.outputs, self.output_states) = tf.nn.bidirectional_dynamic_rnn(self.cell_fw, self.cell_bw, self.rnn_input, self.sequence_length, dtype=tf.float32,scope="tweet")
-
-			# concatenate the backward and forward cells
-			self.concat_outputs = tf.concat(self.outputs, 2)
-
-			# attention layer
-			self.att_context_vector = tf.tanh(tf.tensordot(self.concat_outputs, self.weights["att1-w"], axes=1) + self.bias["att1-w"])
-			self.attentions = tf.nn.softmax(tf.tensordot(self.att_context_vector, self.weights["att1-v"], axes=1))
-			self.attention_output_raw = tf.reduce_sum(self.concat_outputs * tf.expand_dims(self.attentions, -1), 1)
-
-			#reshape the output for the next layers
-			self.attention_output = tf.reshape(self.attention_output_raw, [FLAGS.batch_size, FLAGS.tweet_per_user, 2*FLAGS.rnn_cell_size])
-
-			return self.attention_output
 
 
 
@@ -193,48 +105,3 @@ class network(object):
 
 
 
-	############################################################################################################################
-	def cnn(self, sequence_length, num_classes, vocab_size, embedding_size, filter_sizes, num_filters):
-
-		with tf.device('/device:GPU:0'):
-			# CNN placeholders
-			self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
-			self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
-
-			# Embedding layer
-			with tf.name_scope("embedding"):
-				W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name="W")
-				self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
-				self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
-
-			# Create a convolution + maxpool layer for each filter size
-			pooled_outputs = []
-			for i, filter_size in enumerate(filter_sizes):
-				with tf.name_scope("conv-maxpool-%s" % filter_size):
-					# Convolution Layer
-					filter_shape = [filter_size, embedding_size, 1, num_filters]
-					W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-					b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b-noreg")
-					conv = tf.nn.conv2d(
-					self.embedded_chars_expanded,
-					W,
-					strides=[1, 1, 1, 1],
-					padding="VALID",
-					name="conv")
-					# Apply nonlinearity
-					h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-					# Maxpooling over the outputs
-					pooled = tf.nn.max_pool(
-						h,
-						ksize=[1, sequence_length - filter_size + 1, 1, 1],
-						strides=[1, 1, 1, 1],
-						padding='VALID',
-						name="pool")
-					pooled_outputs.append(pooled)
-
-			# Combine all the pooled features
-			num_filters_total = num_filters * len(filter_sizes)
-			self.h_pool = tf.concat(pooled_outputs, 3)
-			self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
-
-		return self.h_pool_flat
--- parameters.py
+++ parameters.py
@@ -8,7 +8,6 @@ class flags(object):
 		self.training_set_size = 0.8
 
 		#input file paths
-		self.word_embed_path = "/home/cvrg/darg/glove/glove.twitter.27B.200d.txt" #change word embedding size too
 		self.training_data_path = "/home/cvrg/darg/pan_data/pan18-author-profiling-training-2018-02-27"
 		self.test_data_path = "/home/cvrg/darg/pan_data/pan18-author-profiling-test-2018-03-20"
 
@@ -17,16 +16,17 @@ class flags(object):
 		self.model_name = "en-model-0.001-0.0001-3.ckpt"
 		self.log_path = "/home/cvrg/darg/logs/logs_rnn_caption_en.txt"
 
+
+
+		#########################################################################################################################
 		#optimization parameters
 		self.lang = "en"
-		self.model_save_threshold = 0.65
+		self.model_save_threshold = 0.82
 		self.optimize = True #if true below values will be used for hyper parameter optimization, or if testing is run: all the models in model_path will be tested
 							 #if false hyperparameters specified in "model hyperparameters" will be used, and for testing model with model_name and model_path will be used
 		self.l_rate = [0.01, 0.001, 0.0001]
 		self.reg_param = [0.001, 0.0005, 0.0001, 0.00005, 0.00001, 0.000005, 0.000001]
-		self.rnn_cell_sizes = [150, 100, 75, 50, 25]
-
-
+		#self.fc_size = [150, 100, 50] will be used later
 
 
 		#########################################################################################################################
@@ -34,12 +34,9 @@ class flags(object):
 		self.l2_reg_lambda = 0.0001
 		self.learning_rate = 0.001
 		self.num_classes = 2
-			#CNN
-		self.num_filters = 75
-			#RNN
-		self.word_embedding_size = 200
-		self.rnn_cell_size = 50
-
+		self.textrnn_size = 300 #vector sizes
+		self.textcnn_size = 300
+		self.imagernn_size = 50
 
 
 
@@ -47,7 +44,6 @@ class flags(object):
 		##########################################################################################################################
 		# Training parameters
 		self.use_pretrained_model = False
-		self.tweet_per_user = 10
 		self.batch_size = 10
 		self.num_epochs = 25
 		self.evaluate_every = 5
