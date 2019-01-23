391014020745bb3cd1c7e52d49b514fae72ff12e
ErhanSezerer <erhansezerer@gmail.com>
2019-01-23 16:25:28 +0300

    bug fix in typechanger

--- type_changer.py
+++ type_changer.py
@@ -3,7 +3,7 @@ import imghdr
 from PIL import Image
 from tqdm import tqdm
 
-path = "/mnt/671728fd-b9e2-46ed-b18b-9f45f387f63e/turkish_tweets_dataset/turkish_tweets/photo"
+path = "/home/cvrg/darg/pan_data/pan18-author-profiling-test-2018-03-20/en/photo"
 total = 0
 
 for user in tqdm(os.listdir(path)):
@@ -15,17 +15,12 @@ for user in tqdm(os.listdir(path)):
 		if image_type != 'jpeg':
 			print("Found one : " + str(photo))
 			im = Image.open(os.path.join(photo_file, photo))
-			new_name = str(photo.strip().split(".")[0]) + str(".jpeg")
+			new_name = str(photo.strip().split(".")[0]) + str(".") + str(photo.strip().split(".")[1]) + str(".jpeg")
 			im = im.convert("RGB")
+
+			os.remove(os.path.join(photo_file, photo))
 			im.save(os.path.join(photo_file, new_name), "JPEG", quality=100)
 			
-			os.remove(os.path.join(photo_file, photo))
 			total += 1
-			
-		else:
-			im = Image.open(os.path.join(photo_file, photo))
-			new_name = str(photo.strip().split(".")[0]) + str(".jpeg")
-			im.save(os.path.join(photo_file, new_name), "JPEG", quality=100)
-			os.remove(os.path.join(photo_file, photo))
-		
+					
 print(total)
